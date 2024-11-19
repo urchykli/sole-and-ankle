dcb53f8d546b3943d79d2aae80dccb39bf9bf7d3
Lindsay Allison <lindsayallison@macbook-pro.mynetworksettings.com>
2024-11-19 13:49:04 -0500

    Header complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,9 @@ const Header = () => {
     <header>
       <SuperHeader />
       <MainHeader>
-        <Logo />
+        <Side>
+          <Logo />
+        </Side>
         <Nav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
@@ -21,17 +23,29 @@ const Header = () => {
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
         </Nav>
+        <Side />
       </MainHeader>
     </header>
   );
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  padding: 18px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  height: 4.5rem;
+  align-items: baseline;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  margin: 0 48px;
+  display: flex;
+  gap: 48px;
+`;
+
+const Side = styled.div`
+flex: 1;
+`
 
 const NavLink = styled.a`
   font-size: 1.125rem;
